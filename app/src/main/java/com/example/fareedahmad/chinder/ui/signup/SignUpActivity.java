3220b00f21951ff7c2cbad7e7266ffd173ab86b5
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2019-02-17 00:00:21 -0500

    User type drop down

--- app/src/main/java/com/example/fareedahmad/chinder/ui/signup/SignUpActivity.java
+++ app/src/main/java/com/example/fareedahmad/chinder/ui/signup/SignUpActivity.java
@@ -4,16 +4,48 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.fareedahmad.chinder.R;
 import com.example.fareedahmad.chinder.ui.signin.SignInActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private String userType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        spinnerInit();
+    }
+
+    public void spinnerInit(){
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.userType);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Donor");
+        categories.add("Organization");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
     }
 
     public void signin(View view) {
@@ -21,4 +53,14 @@ public class SignUpActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        userType = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
