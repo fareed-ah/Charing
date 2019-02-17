cc17b5df108f5b82e6d0ecd280956710514305fe
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2019-02-16 21:46:43 -0500

    refactor signup

--- app/src/main/java/com/example/fareedahmad/chinder/MainActivity.java
+++ app/src/main/java/com/example/fareedahmad/chinder/MainActivity.java
@@ -15,12 +15,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signin(View view) {
-        Intent intent = new Intent(this, SignInActivity.class);
+        Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
 
     public void signup(View view){
-        Intent intent = new Intent(this, SignInActivity.class);
+        Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
 }
